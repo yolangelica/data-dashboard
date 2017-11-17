cfa6df601f2ee3a27d31e50cbcc7b29c8ea44a87
Niennis <estefania.osses.v@gmail>
2017-11-16 23:11:16 -0300

    comentarios en js por select hechos y seccion notas en html

--- js/app.js
+++ js/app.js
@@ -6,12 +6,18 @@
 console.log(data);
 
 // MENU PARA SELECCIONAR CIUDAD
+// Llamamos al elemento contenedor
 var seleccionar_ciudad = document.getElementById('seleccionar_ciudad');
 
+// Creamos un select para elegir ciudad y le damos atributos
 var select_ciudad = document.createElement('select');
 select_ciudad.setAttribute('name','select_ciudad');
 select_ciudad.setAttribute('id','select_ciudad');
 
+// Creamos un elemento option por ciudad
+// Le damos atributo value
+// Creamos un textnode para darle nombre a cada opción
+// Agregamos el textnode a la opción
 var option_lima = document.createElement('option')
 option_lima.setAttribute('value','value1');
 var texto_lima = document.createTextNode('LIMA');
@@ -32,22 +38,32 @@ option_mex.setAttribute('value','value4');
 var texto_mex = document.createTextNode('CIUDAD DE MÉXICO');
 option_mex.appendChild(texto_mex);
 
+// Agregamos las opciones al elemento select
+// Agregamos el elemento select a nuestro elemento en html
 select_ciudad.appendChild(option_lima);
 select_ciudad.appendChild(option_aqp);
 select_ciudad.appendChild(option_stgo);
 select_ciudad.appendChild(option_mex);
 seleccionar_ciudad.appendChild(select_ciudad);
 
+// Determinamos el valor a mostrar al cargar la página,
+// que corresponde a nuestra ciudad, Santiago
 select_ciudad.selectedIndex = "2";
 
-// MENU PARA SELECCIONAR GENERACION
 
+// MENU PARA SELECCIONAR GENERACION
+// Llamamos al elemento contenedor
 var seleccionar_gen = document.getElementById('seleccionar_generacion');
 
+// Creamos un select para elegir generación y le damos atributos
 var select_gen = document.createElement('select');
 select_gen.setAttribute('name','select_generacion');
 select_gen.setAttribute('id','select_generacion');
 
+// Creamos un elemento option por generación
+// Le damos atributo value
+// Creamos un textnode para darle nombre a cada opción
+// Agregamos el textnode a la opción
 var option_gen20151 = document.createElement('option')
 option_gen20151.setAttribute('value','value1');
 var texto_gen20151 = document.createTextNode('2015-I');
@@ -73,6 +89,8 @@ option_gen20172.setAttribute('value','value4');
 var texto_gen20172 = document.createTextNode('2017-II');
 option_gen20172.appendChild(texto_gen20172);
 
+// Agregamos las opciones al elemento select
+// Agregamos el elemento select a nuestro elemento en html
 select_gen.appendChild(option_gen20151);
 select_gen.appendChild(option_gen20161);
 select_gen.appendChild(option_gen20162);
@@ -80,30 +98,6 @@ select_gen.appendChild(option_gen20171);
 select_gen.appendChild(option_gen20172);
 seleccionar_gen.appendChild(select_gen);
 
+// Determinamos el valor a mostrar al cargar la página,
+// que corresponde a nuestra generación 2017-II
 select_gen.selectedIndex = "4";
-
-
-
-
-
-
-/*
-var lista_ciudad = document.createElement('ul');
-
-var option_stgo = document.getElementById('option_stgo');
-
-var li_stgo = document.createElement('li')
-var stgo = document.createTextNode('Santiago');
-li_stgo.appendChild(stgo);
-
-var li_mex = document.createElement('li');
-var mex = document.createTextNode('Ciudad de México');
-li_mex.appendChild(mex);
-
-lista_ciudad.appendChild(li_lima);
-lista_ciudad.appendChild(li_aqp);
-lista_ciudad.appendChild(li_stgo);
-lista_ciudad.appendChild(li_mex);
-
-seleccionar_ciudad.appendChild(lista_ciudad);
-*/
